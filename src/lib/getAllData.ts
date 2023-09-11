4726cf572799901c05708df211c1882bd4b2eae9
Joaco Montesano <montesano1994@gmail.com>
2023-09-11 20:20:18 -0300

    updated svgs

--- src/lib/getAllData.ts
+++ src/lib/getAllData.ts
@@ -1,20 +1,21 @@
-"use server"
+"use server";
 import { ProductsList } from "../types/products";
 
 export async function getData(): Promise<ProductsList> {
 
-    const sheetId = "1r74G-LQCSEDh5_O6mnDecMoi8BMvzStdt4rNLu9zqkQ";
-    const apiKey = "AIzaSyBLFucdHwI51bvInRnmig4Tl2fglpqYffk";
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/productList?key=${apiKey}`;
-    const res = await fetch(url, {
-      next: { revalidate: 60 },
-    });
-  
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-  
-    const result = await res.json();
-    const values: ProductsList = result.values || [];
-    return values;
+  const SHEET_ID = process.env.API_URL;
+  const API_KEY = process.env.API_URL;
+
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/productList?key=${API_KEY}`;
+  const res = await fetch(url, {
+    next: { revalidate: 60 },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
+
+  const result = await res.json();
+  const values: ProductsList = result.values || [];
+  return values;
+}
