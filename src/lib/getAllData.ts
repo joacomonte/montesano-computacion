d8eeb179374ecd16a24403b7a82730e2614079c9
Joaco Montesano <montesano1994@gmail.com>
2023-09-11 19:16:21 -0300

    minor fixes

--- src/lib/getAllData.ts
+++ src/lib/getAllData.ts
@@ -18,23 +18,3 @@ export async function getData(): Promise<ProductsList> {
     const values: ProductsList = result.values || [];
     return values;
   }
-
-  // export async function getData(): Promise<ProductsList> {
-
-//     const sheetId = "1r74G-LQCSEDh5_O6mnDecMoi8BMvzStdt4rNLu9zqkQ";
-//     const apiKey = "AIzaSyBLFucdHwI51bvInRnmig4Tl2fglpqYffk";
-//     const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/productList?key=${apiKey}`;
-//     const res = await fetch(url);
-
-//     if (!res.ok) {
-//         throw new Error("Failed to fetch data");
-//     }
-
-//     const result = await res.json();
-
-//     if (!result.values || !Array.isArray(result.values)) {
-//         throw new Error("Unexpected response format");
-//     }
-
-//     return result.values as ProductsList;
-//   }
