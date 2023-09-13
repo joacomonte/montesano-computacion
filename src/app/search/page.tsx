a1ab83b06d855887820ba9660a331fe77ec89c7d
Joaco Montesano <montesano1994@gmail.com>
2023-09-13 12:17:03 -0300

    udpated search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ProductCard from "@/components/ProductCard/ProductCard";
 import index from "@/globals";
 import useFilterData from "@/hooks/useFilterData";
 import { getData } from "@/lib/getAllData";
@@ -7,11 +8,13 @@ import { ProductsList } from "@/types/products";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import styles from "./page.module.css";
+
 export default function SearchPage() {
   const [data, setData] = useState<ProductsList>();
   const [searchTerm, setSearchTerm] = useState("");
 
-  const search = useSearchParams().get("query") || "";
+  const queryTerm = useSearchParams().get("query") || "";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,52 +22,39 @@ export default function SearchPage() {
       setData(result);
     };
     fetchData();
-    setSearchTerm(search);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    setSearchTerm(queryTerm);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
 
   const filteredData = useFilterData(data || [], searchTerm);
 
   return (
-    <div
-      style={{
-        marginTop: "150px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
-      }}
-    >
-      <input
-        //   className={styles.searchInput}
-        id="searchInput"
-        type="text"
-        placeholder="Buscar..."
-        aria-label="Busqueda"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      {/* <h1>{data ? `Resultados de: ${searchTerm}` : "sin resultados"}</h1> */}
-      <h3
-        style={{
-          flex: 1,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          textAlign: "center",
-          margin: "50px",
-        }}
-      >
-        {data ? (
-          filteredData
-            .slice(0, 15)
-            .map((product) => <span key={product[index.ID]}> {product[index.TITLE]} - {product[index.PRICE]}</span>)
-        ) : (
-          <span>Sin resultados</span>
-        )}
-      </h3>
+    <div className={styles.pageContent}>
+      <div className={styles.searchContainer}>
+        <label className={styles.searchLabel}>Busqueda global</label>
+        <input
+          className={styles.searchInput}
+          id="searchInput"
+          type="text"
+          placeholder="Buscar..."
+          aria-label="Busqueda"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+      <div className="productsContainer">
+        {filteredData.map((product) => (
+          <ProductCard
+            key={product[index.ID]}
+            title={product[index.TITLE]}
+            img={product[index.IMG]}
+            price={product[index.PRICE]}
+            oldPrice={product[index.OLD_PRICE]}
+            stock={product[index.STOCK]}
+          />
+        ))}
+      </div>
     </div>
   );
 }
