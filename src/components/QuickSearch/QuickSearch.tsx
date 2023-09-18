88bbe271e3e1c9a6839ec5728a0e33ea2d236374
Joaco Montesano <montesano1994@gmail.com>
2023-09-18 10:55:15 -0300

    quicksearch not rendered on search route

--- src/components/QuickSearch/QuickSearch.tsx
+++ src/components/QuickSearch/QuickSearch.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { FC, use, useEffect, useRef, useState } from "react";
 import styles from "./QuickSearch.module.css";
 import { ProductsList } from "@/types/products";
 import useFilterData from "@/hooks/useFilterData";
 import useClickOutside from "@/hooks/useOutsideClick";
 import { getData } from "@/lib/getAllData";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import index from "@/globals";
 
 interface QuickSearchProps {
@@ -15,6 +16,10 @@ interface QuickSearchProps {
 
 const QuickSearch: FC<QuickSearchProps> = () => {
   const router = useRouter();
+  
+  const currentPage = usePathname();
+
+  const [shouldRender, setShouldRender] = useState<boolean>(true);
 
   const [data, setData] = useState<ProductsList>();
 
@@ -23,11 +28,18 @@ const QuickSearch: FC<QuickSearchProps> = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
+    if (currentPage === "/search") {
+      setShouldRender(false);
+    } else {
+      setShouldRender(true);
+    }
+  }, [currentPage]);
+
+  useEffect(() => {
     const fetchData = async () => {
       const result = await getData();
       setData(result);
     };
-
     fetchData();
   }, []);
 
@@ -45,48 +57,53 @@ const QuickSearch: FC<QuickSearchProps> = () => {
     clearData();
   };
 
-
-
   return (
-    <div className={styles.componentContainer} ref={divRef}>
-      <form onSubmit={handleSearch}>
-        <input
-          className={styles.searchInput}
-          id="searchInput"
-          type="text"
-          placeholder="Buscar..."
-          aria-label="Busqueda"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </form>
-      <button
-        type="submit"
-        className={styles.searchIcon}
-        aria-label="Search"
-        role="button"
-        onClick={handleSearch}
-      >
-        &#128269;
-      </button>
-      {searchTerm && searchTerm.trim() !== "" ? (
-        filteredData.length > 0 ? (
-          <ul className={styles.cardList}>
-            {filteredData.slice(0, 6).map((product) => (
-              <li key={product[index.ID]} className={styles.card}>
-                <Link href={`/product/${product[index.ID]}`} onClick={clearData}>
-                  {product[index.TITLE]} <b>{product[index.PRICE]}</b>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <ul className={styles.cardList}>
-            <li className={styles.card}>Sin resultados</li>
-          </ul>
-        )
-      ) : null}
-    </div>
+    <>
+      {shouldRender && (
+        <div className={styles.componentContainer} ref={divRef}>
+          <form onSubmit={handleSearch}>
+            <input
+              className={styles.searchInput}
+              id="searchInput"
+              type="text"
+              placeholder="Buscar..."
+              aria-label="Busqueda"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </form>
+          <button
+            type="submit"
+            className={styles.searchIcon}
+            aria-label="Search"
+            role="button"
+            onClick={handleSearch}
+          >
+            &#128269;
+          </button>
+          {searchTerm && searchTerm.trim() !== "" ? (
+            filteredData.length > 0 ? (
+              <ul className={styles.cardList}>
+                {filteredData.slice(0, 6).map((product) => (
+                  <li key={product[index.ID]} className={styles.card}>
+                    <Link
+                      href={`/product/${product[index.ID]}`}
+                      onClick={clearData}
+                    >
+                      {product[index.TITLE]} <b>{product[index.PRICE]}</b>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <ul className={styles.cardList}>
+                <li className={styles.card}>Sin resultados</li>
+              </ul>
+            )
+          ) : null}
+        </div>
+      )}
+    </>
   );
 };
 
