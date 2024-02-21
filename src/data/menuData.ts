37f2f1855a151d132dec7a9328c7282a95da45e3
Joaco Montesano <70350164+joacomonte@users.noreply.github.com>
2024-02-21 15:34:43 -0300

    creo que funciona bien

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,8 +1,21 @@
 export const menuData = [
     {
+        id: "02",
+        label: "Equipos",
+        children: [
+            { id: "0201", label: "Todos", href: "/equipos/todos" },
+            { id: "0202", label: "Notebooks", href: "/equipos/notebook" },
+            { id: "0203", label: "PC Gamer", href: "/equipos/pc-gamer" },
+            { id: "0204", label: "PC Hogar", href: "/equipos/pc-hogar" }
+            { id: "0204", label: "Monitores", href: "/equipos/monitores" }
+            { id: "0204", label: "Impresoras", href: "/equipos/impresoras" }
+        ]
+    },
+    {
         id: "01",
         label: "Componentes",
         children: [
+            { id: "0101", label: "Todos", href: "/componentes/todos" },
             { id: "0101", label: "Almacenamiento", href: "/componentes/almacenamiento" },
             { id: "0107", label: "Coolers", href: "/componentes/cooler" },
             { id: "0106", label: "Fuentes", href: "/componentes/fuentes" },
@@ -11,74 +24,82 @@ export const menuData = [
             { id: "0108", label: "Mothers", href: "/componentes/mothers" },
             { id: "0104", label: "Placas de Video", href: "/componentes/placa-video" },
             { id: "0105", label: "Procesadores", href: "/componentes/procesadores" },
-            { id: "0103", label: "Coolers", href: "/componentes/cooler" },
-        ]
-    },
-    {
-        id: "02",
-        label: "Notebooks",
-        children: [
-            { id: "0201", label: "Todos", href: "/notebooks/todos" },
-            { id: "0202", label: "Oficina", href: "/notebooks/oficina" },
-            { id: "0203", label: "Gamer", href: "/notebooks/gamer" },
-            { id: "0204", label: "Extreme", href: "/notebooks/extreme" }
         ]
     },
     {
         id: "03",
-        label: "Monitores",
+        label: "Perisfericos",
         children: [
-            { id: "0301", label: "Todos", href: "/monitores/todos" },
-            { id: "0302", label: "Oficina", href: "/monitores/oficina" },
-            { id: "0303", label: "Gamer", href: "/monitores/gamer" }
+            { id: "0301", label: "Todos", href: "/perisfericos/todos" },
+            { id: "0302", label: "Auriculares", href: "/perisfericos/auriculares" },
+            { id: "0303", label: "Mouse", href: "/perisfericos/mouse" },
+            { id: "0303", label: "Parlantes", href: "/perisfericos/parlantes" },
+            { id: "0303", label: "Teclados", href: "/perisfericos/teclados" },
+            { id: "0303", label: "Camaras", href: "/perisfericos/camaras" },
+            { id: "0303", label: "Cables", href: "/perisfericos/cables" },
+            { id: "0303", label: "Adaptadores", href: "/perisfericos/adaptadores" },
+            { id: "0303", label: "Pads", href: "/perisfericos/pads" },
+            { id: "0303", label: "Otros", href: "/perisfericos/varios" },
         ]
     },
     {
         id: "04",
-        label: "Impresoras",
+        label: "Conectividad",
         children: [
-            { id: "0401", label: "Todos", href: "/impresoras/todos" },
-            { id: "0402", label: "Cartucho", href: "/impresoras/cartucho" },
-            { id: "0403", label: "Botellas de tinta", href: "/impresoras/botellas-de-tinta" }
+            { id: "0401", label: "Todos", href: "/conectividad/todos" },
+            { id: "0402", label: "Wi-Fi", href: "/conectividad/wi-fi" },
+            { id: "0402", label: "Switches", href: "/conectividad/switch" },
+            { id: "0402", label: "Routers", href: "/conectividad/router" },
+            { id: "0402", label: "Extensores", href: "/conectividad/extensores" },
+            { id: "0402", label: "Bluetooth", href: "/conectividad/bluetooth" },
+            { id: "0402", label: "Placa de Red", href: "/conectividad/placa-red" },
         ]
     },
     {
         id: "05",
-        label: "Perisfericos",
+        label: "Insumos",
         children: [
-            { id: "0501", label: "Todos", href: "/perisfericos/todos" },
-            { id: "0502", label: "Parlantes", href: "/perisfericos/parlantes" },
-            { id: "0503", label: "Teclados", href: "/perisfericos/teclados" },
-            { id: "0504", label: "Mouse", href: "/perisfericos/mouse" }
+            { id: "0501", label: "Todos", href: "/insumos/todos" },
+            { id: "0502", label: "Epson", href: "/insumos/epson-originales" },
+            { id: "0502", label: "Epson Alternativos", href: "/insumos/epson-alternativos" },
+            { id: "0502", label: "HP", href: "/insumos/hp-originales" },
+            { id: "0502", label: "HP Alternativos", href: "/insumos/hp-alternativos" },
+            { id: "0503", label: "Toners", href: "/insumos/toners" },
+            { id: "0504", label: "Resmas", href: "/insumos/resmas" },
+            { id: "0504", label: "Aerosoles", href: "/insumos/aerosoles" },
+            { id: "0504", label: "Pilas", href: "/insumos/pilas" },
+            { id: "0504", label: "Grasa", href: "/insumos/grasa" },
         ]
     },
-    {
-        id: "06",
-        label: "Componentes",
-        children: [
-            { id: "0601", label: "Todos", href: "/componentes/todos" },
-            { id: "0602", label: "Fuente", href: "/componentes/fuente" },
-            { id: "0603", label: "Mother", href: "/componentes/mother" },
-            { id: "0604", label: "CPU", href: "/componentes/cpu" },
-            { id: "0605", label: "GPU", href: "/componentes/gpu" },
-            { id: "0606", label: "Almacenamiento", href: "/componentes/almacenamiento" }
-        ]
-    },
-    {
-        id: "07",
-        label: "Adaptadores/Cables",
-        children: [
-            { id: "0701", label: "Todos", href: "/adaptadores-cables/todos" },
-            { id: "0702", label: "Otros", href: "/adaptadores-cables/otros" }
-        ]
-    },
-    {
-        id: "08",
-        label: "Sillas",
-        children: [
-            { id: "0801", label: "Todos", href: "/sillas/todos" },
-            { id: "0802", label: "Oficina", href: "/sillas/oficina" },
-            { id: "0803", label: "Gamer", href: "/sillas/gamer" }
-        ]
-    }
 ];
+
+
+    // {
+    //     id: "06",
+    //     label: "Componentes",
+    //     children: [
+    //         { id: "0601", label: "Todos", href: "/componentes/todos" },
+    //         { id: "0602", label: "Fuente", href: "/componentes/fuente" },
+    //         { id: "0603", label: "Mother", href: "/componentes/mother" },
+    //         { id: "0604", label: "CPU", href: "/componentes/cpu" },
+    //         { id: "0605", label: "GPU", href: "/componentes/gpu" },
+    //         { id: "0606", label: "Almacenamiento", href: "/componentes/almacenamiento" }
+    //     ]
+    // },
+    // {
+    //     id: "07",
+    //     label: "Adaptadores/Cables",
+    //     children: [
+    //         { id: "0701", label: "Todos", href: "/adaptadores-cables/todos" },
+    //         { id: "0702", label: "Otros", href: "/adaptadores-cables/otros" }
+    //     ]
+    // },
+    // {
+    //     id: "08",
+    //     label: "Sillas",
+    //     children: [
+    //         { id: "0801", label: "Todos", href: "/sillas/todos" },
+    //         { id: "0802", label: "Oficina", href: "/sillas/oficina" },
+    //         { id: "0803", label: "Gamer", href: "/sillas/gamer" }
+    //     ]
+    // }
