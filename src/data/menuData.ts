3f6390cf39ccaed3bfe93295c94b91cfa3c27fa8
Joaco Montesano <70350164+joacomonte@users.noreply.github.com>
2024-02-21 15:35:23 -0300

    id updated

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,45 +1,45 @@
 export const menuData = [
     {
+        id: "01",
+        label: "Componentes",
+        children: [
+            { id: "0101", label: "Todos", href: "/componentes/todos" },
+            { id: "0102", label: "Almacenamiento", href: "/componentes/almacenamiento" },
+            { id: "0103", label: "Coolers", href: "/componentes/cooler" },
+            { id: "0104", label: "Fuentes", href: "/componentes/fuentes" },
+            { id: "0105", label: "Gabinetes", href: "/componentes/gabinetes" },
+            { id: "0106", label: "Memorias", href: "/componentes/memorias" },
+            { id: "0107", label: "Mothers", href: "/componentes/mothers" },
+            { id: "0108", label: "Placas de Video", href: "/componentes/placa-video" },
+            { id: "0109", label: "Procesadores", href: "/componentes/procesadores" },
+        ]
+    },
+    {
         id: "02",
         label: "Equipos",
         children: [
             { id: "0201", label: "Todos", href: "/equipos/todos" },
             { id: "0202", label: "Notebooks", href: "/equipos/notebook" },
             { id: "0203", label: "PC Gamer", href: "/equipos/pc-gamer" },
-            { id: "0204", label: "PC Hogar", href: "/equipos/pc-hogar" }
-            { id: "0204", label: "Monitores", href: "/equipos/monitores" }
-            { id: "0204", label: "Impresoras", href: "/equipos/impresoras" }
-        ]
-    },
-    {
-        id: "01",
-        label: "Componentes",
-        children: [
-            { id: "0101", label: "Todos", href: "/componentes/todos" },
-            { id: "0101", label: "Almacenamiento", href: "/componentes/almacenamiento" },
-            { id: "0107", label: "Coolers", href: "/componentes/cooler" },
-            { id: "0106", label: "Fuentes", href: "/componentes/fuentes" },
-            { id: "0109", label: "Gabinetes", href: "/componentes/gabinetes" },
-            { id: "0102", label: "Memorias", href: "/componentes/memorias" },
-            { id: "0108", label: "Mothers", href: "/componentes/mothers" },
-            { id: "0104", label: "Placas de Video", href: "/componentes/placa-video" },
-            { id: "0105", label: "Procesadores", href: "/componentes/procesadores" },
+            { id: "0204", label: "PC Hogar", href: "/equipos/pc-hogar" },
+            { id: "0205", label: "Monitores", href: "/equipos/monitores" },
+            { id: "0206", label: "Impresoras", href: "/equipos/impresoras" }
         ]
     },
     {
         id: "03",
-        label: "Perisfericos",
+        label: "Perisféricos",
         children: [
             { id: "0301", label: "Todos", href: "/perisfericos/todos" },
             { id: "0302", label: "Auriculares", href: "/perisfericos/auriculares" },
             { id: "0303", label: "Mouse", href: "/perisfericos/mouse" },
-            { id: "0303", label: "Parlantes", href: "/perisfericos/parlantes" },
-            { id: "0303", label: "Teclados", href: "/perisfericos/teclados" },
-            { id: "0303", label: "Camaras", href: "/perisfericos/camaras" },
-            { id: "0303", label: "Cables", href: "/perisfericos/cables" },
-            { id: "0303", label: "Adaptadores", href: "/perisfericos/adaptadores" },
-            { id: "0303", label: "Pads", href: "/perisfericos/pads" },
-            { id: "0303", label: "Otros", href: "/perisfericos/varios" },
+            { id: "0304", label: "Parlantes", href: "/perisfericos/parlantes" },
+            { id: "0305", label: "Teclados", href: "/perisfericos/teclados" },
+            { id: "0306", label: "Camaras", href: "/perisfericos/camaras" },
+            { id: "0307", label: "Cables", href: "/perisfericos/cables" },
+            { id: "0308", label: "Adaptadores", href: "/perisfericos/adaptadores" },
+            { id: "0309", label: "Pads", href: "/perisfericos/pads" },
+            { id: "0310", label: "Otros", href: "/perisfericos/varios" },
         ]
     },
     {
@@ -48,11 +48,11 @@ export const menuData = [
         children: [
             { id: "0401", label: "Todos", href: "/conectividad/todos" },
             { id: "0402", label: "Wi-Fi", href: "/conectividad/wi-fi" },
-            { id: "0402", label: "Switches", href: "/conectividad/switch" },
-            { id: "0402", label: "Routers", href: "/conectividad/router" },
-            { id: "0402", label: "Extensores", href: "/conectividad/extensores" },
-            { id: "0402", label: "Bluetooth", href: "/conectividad/bluetooth" },
-            { id: "0402", label: "Placa de Red", href: "/conectividad/placa-red" },
+            { id: "0403", label: "Switches", href: "/conectividad/switch" },
+            { id: "0404", label: "Routers", href: "/conectividad/router" },
+            { id: "0405", label: "Extensores", href: "/conectividad/extensores" },
+            { id: "0406", label: "Bluetooth", href: "/conectividad/bluetooth" },
+            { id: "0407", label: "Placa de Red", href: "/conectividad/placa-red" },
         ]
     },
     {
@@ -61,17 +61,18 @@ export const menuData = [
         children: [
             { id: "0501", label: "Todos", href: "/insumos/todos" },
             { id: "0502", label: "Epson", href: "/insumos/epson-originales" },
-            { id: "0502", label: "Epson Alternativos", href: "/insumos/epson-alternativos" },
-            { id: "0502", label: "HP", href: "/insumos/hp-originales" },
-            { id: "0502", label: "HP Alternativos", href: "/insumos/hp-alternativos" },
-            { id: "0503", label: "Toners", href: "/insumos/toners" },
-            { id: "0504", label: "Resmas", href: "/insumos/resmas" },
-            { id: "0504", label: "Aerosoles", href: "/insumos/aerosoles" },
-            { id: "0504", label: "Pilas", href: "/insumos/pilas" },
-            { id: "0504", label: "Grasa", href: "/insumos/grasa" },
+            { id: "0503", label: "Epson Alternativos", href: "/insumos/epson-alternativos" },
+            { id: "0504", label: "HP", href: "/insumos/hp-originales" },
+            { id: "0505", label: "HP Alternativos", href: "/insumos/hp-alternativos" },
+            { id: "0506", label: "Toners", href: "/insumos/toners" },
+            { id: "0507", label: "Resmas", href: "/insumos/resmas" },
+            { id: "0508", label: "Aerosoles", href: "/insumos/aerosoles" },
+            { id: "0509", label: "Pilas", href: "/insumos/pilas" },
+            { id: "0510", label: "Grasa", href: "/insumos/grasa" },
         ]
     },
 ];
+
 
 
     // {
