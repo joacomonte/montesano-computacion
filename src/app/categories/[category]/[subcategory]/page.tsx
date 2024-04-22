02653066b54b8b32be688142994d61986bfb678a
Joaco Montesano <montesano1994@gmail.com>
2024-04-22 10:20:08 -0300

    varios cambios, me perdi

--- src/app/categories/[category]/[subcategory]/page.tsx
+++ src/app/categories/[category]/[subcategory]/page.tsx
@@ -8,19 +8,13 @@ type Params = {
   };
 };
 
-export default async function Subcategory({
-  params: { category, subcategory },
-}: Params) {
+export default async function Subcategory({ params: { category, subcategory } }: Params) {
   let data = await getByCategory(category, subcategory);
 
   return (
     <div className="flex flex-col items-center w-full mt-40">
-      <div className="w-9/10 flex justify-center items-center flex-wrap gap-5 md:gap-4 md:w-full md:text-sm">
-        {data && data.length > 0 ? (
-          <ProductsList data={data} />
-        ) : (
-          <h3 className="text-2xl font-bold"> No se encontraron productos en esta categoria :(</h3>
-        )}
+      <div className="flex flex-wrap items-center justify-center gap-5 w-9/10 ">
+        {data && data.length > 0 ? <ProductsList data={data} /> : <h3 className="text-2xl font-bold"> No se encontraron productos en esta categoria :(</h3>}
       </div>
     </div>
   );
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -13,18 +13,19 @@ export default async function Subcategory({ params: { id } }: Params) {
 
   return (
     <div className="flex flex-col items-center w-full mt-40">
-      {product ?
-      <ProductCard
-        key={product[index.ID]}
-        title={product[index.TITLE]}
-        img={product[index.IMG]}
-        price={product[index.PRICE]}
-        oldPrice={product[index.OLD_PRICE]}
-        stock={product[index.STOCK]}
-      />
-      :
-      null
-      }
+      {product ? (
+        <ProductCard
+          key={product[index.ID]}
+          title={product[index.TITLE]}
+          img={product[index.IMG]}
+          price={product[index.PRICE]}
+          oldPrice={product[index.OLD_PRICE]}
+          stock={product[index.STOCK]}
+          description={product[index.DESCRIPTION]}
+          cuotas1={product[index.CUOTAS1]}
+          cuotas2={product[index.CUOTAS2]}
+        />
+      ) : null}
     </div>
   );
 }
