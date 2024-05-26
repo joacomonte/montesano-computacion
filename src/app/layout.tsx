a2b7f4e4040bfc7c606393991ad12a3ff481edbc
Joaco Montesano <montesano1994@gmail.com>
2024-05-26 16:09:10 -0300

    updated viewport syntax for new next.js version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import SideMenu from "../components/SideMenu/SideMenu";
 import { menuData } from "../data/menuData";
@@ -12,13 +12,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Montesano Computacion",
   description: "Developed by Montech",
-  //to avoid zoom on inputs
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
+
 };
+
+export const viewport: Viewport = { width: "device-width", initialScale: 1, maximumScale: 1 };
+
 
 export default async function RootLayout({
   children,
