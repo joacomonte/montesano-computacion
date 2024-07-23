9811cbb3ab25818fde60954655269ad71fe955d3
Joaco Montesano <70350164+joacomonte@users.noreply.github.com>
2024-07-23 16:37:40 -0300

    test4

--- src/app/categories/todos/page.tsx
+++ src/app/categories/todos/page.tsx
@@ -1,18 +1,19 @@
 import ProductsList from "@/components/ProductsList/ProductsList";
-import { getData } from "@/lib/getAllData";
+// import { getData } from "@/lib/getAllData";
 
 export default async function AllSubCategories() {
-  let data = await getData();
+  // let data = await getData();
 
   return (
     <div className="flex flex-col items-center w-full mt-40">
-      <div className="w-9/10 flex justify-center items-center flex-wrap gap-5 md:gap-4 md:w-full md:text-sm">
+      hola
+      {/* <div className="w-9/10 flex justify-center items-center flex-wrap gap-5 md:gap-4 md:w-full md:text-sm">
         {data && data.length > 0 ? (
           <ProductsList data={data} />
         ) : (
           <h3 className="text-2xl font-bold"> No se encontraron productos en esta categoria :(</h3>
         )}
-      </div>
+      </div> */}
     </div>
   );
 }
